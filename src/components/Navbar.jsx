cb407d3cc4d465ff9a844a8f0e14a25ec7c73c2b
Joshaw-k <awinrin4bless@yahoo.com>
2023-12-07 14:23:23 +0100

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
 import React from "react";
+import cartesi from "../assets/cartesi.jpeg";
 
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 max-w-3xl mx-auto">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">StudentDB</a>
+        <a className="btn btn-ghost text-xl">
+          <span>
+            <img src={cartesi} alt="" className="w-8 rounded-full" />
+          </span>
+          CartesiDB
+        </a>
       </div>
       <div className="flex-none">
-        <ConnectWallet />
+        <h1>ConnectWallet</h1>
       </div>
     </div>
   );
